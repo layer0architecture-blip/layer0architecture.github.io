c6bbef49ed2847b2ef5c71deedd48ef2e310be52
layer0architecture-blip <layer0architecture@gmail.com>
2026-08-18 23:49:25 +0000

    Usar anon public del proyecto correcto

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
     url: 'https://vxsdinfhlwyhhtworugr.supabase.co',
-    anonKey: 'sb_publishable_5uY9dgxrlkol7CkWL00z2g_KqPuEZ10'
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4c2RpbmZobHd5aGh0d29ydWdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwODg2NTMsImV4cCI6MjEwMjY2NDY1M30.m7E4HpEeR9Sc4YfB4vNN85M9pFFPbgd9jyVLfoamSgg'
 };
