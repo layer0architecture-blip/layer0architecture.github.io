de8db6190a3486dbd095b4f5e6bb863e08b4cfa0
layer0architecture-blip <layer0architecture@gmail.com>
2026-08-18 23:47:26 +0000

    Corregir URL del proyecto Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-    url: 'https://btbfcbttcuayydywsokx.supabase.co',
+    url: 'https://vxsdinfhlwyhhtworugr.supabase.co',
     anonKey: 'sb_publishable_5uY9dgxrlkol7CkWL00z2g_KqPuEZ10'
 };
